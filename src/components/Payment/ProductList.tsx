0aadc93d788526552c8b0420c2fe83c80b444714
kimeunseo58 <alan0221@naver.com>
2024-02-27 18:20:32 +0900

    배포 (#16)
    * feat: Env modify (#8)
    
    * fix : env 상태 확인
    
    * feat: 환경변수 설정
    
    * feat: 신상품 수령 레이아웃 (#9)
    
    * feat: 제품 정보 레이아웃
    
    * feat: 그래프 설정
    
    * feat: 신제품 신청 목록
    
    * feat: 모달창 레이아웃
    
    * feat: 쿠키 설정
    
    * feat: 소켓 통신 (#10)
    
    * feat: 버튼 통한 내역 추가
    
    * feat: 신상품 수령 확인
    
    * feat: 소켓 통신
    
    * fix: useEffect 경고
    
    * fix :로그 제거 (#11)
    
    * fix: 소켓 주소 ssl
    
    * feat: socket msg
    
    * feat: 물품 추가 버튼 (#15)

--- src/components/Payment/ProductList.tsx
+++ src/components/Payment/ProductList.tsx
@@ -1,6 +1,7 @@
 import React,{useEffect, useMemo,useState} from "react"
 import 'css/Payment/ProductList.css'
 import CouponModal from "components/Modal/CouponModal"
+import NoListModal from "components/Modal/NoListModal";
 import { useDispatch } from 'react-redux'
 import { setCoupon, setCouponId } from "store/reducer/couponMoney"
 import { useSelector } from 'react-redux';
@@ -13,6 +14,7 @@ const ProductList =() =>{
     const totalList = useSelector((state : RootState) => state.paymentList);
     const dispatch = useDispatch();
     const [data,setData] =useState([])
+    const [falseModal, setFalseModal] = useState(false)
     const [modal,setModal] =useState(false)
     const [total, setTotal] = useState({
         cnt : 0,
@@ -35,22 +37,36 @@ const ProductList =() =>{
         };
       }, [socket]);
 
+    useEffect(() => {
+        socket.onopen = (event) => {
+          console.log("WebSocket connection opened:", event);
+        };
+        return () => {
+          socket.onclose = function (event) {
+            console.log("WebSocket connection closed:", event);
+          };
+        };
+      }, [socket]);
+
     socket.onmessage=(e)=>{
         const getCouponList= async() =>{
             try{
                 const res = await get(`${process.env.REACT_APP_API_URL}/pos/coupon/list/${e.data}`);
-                const dataWithIds = res.map((item, index) => ({
-                    ...item,
-                    id: index + 1
-                }));
-                setData(dataWithIds);
+                if(res.length() === 0) setFalseModal(true)
+                else{
+                    const dataWithIds = res.map((item, index) => ({
+                        ...item,
+                        id: index + 1
+                    }));
+                    setData(dataWithIds);
+                    setModal(true)
+                }
             }
             catch(e){
                 return
             }
         }
         getCouponList()
-        setModal(true)
     }
     useEffect(()=>{
         let calCnt = 0
@@ -73,49 +89,33 @@ const ProductList =() =>{
         <div className="ProductList">
             <div className="ProductListTop">
                 <div className="TopBar">
-                    <p style={{width:'10%'}}>&nbsp;작업</p>
-                    <hr/>
-                    <p style={{width:'10%'}}>상품ID</p>
-                    <hr/>
-                    <p style={{width:'38%'}}>상품명</p>
-                    <hr/>
-                    <p style={{width:'14%'}}>단가</p>
-                    <hr/>
-                    <p style={{width:'6%'}}>수량</p>
-                    <hr/>
-                    <p style={{width:'17%'}}>합계</p>
+                    <p className="Width10P">작업</p>
+                    <p className="Width10P">상품ID</p>
+                    <p className="Width40P">상품명</p>
+                    <p className="Width15P">단가</p>
+                    <p className="Width10P">수량</p>
+                    <p className="Width15P_last">합계</p>
                 </div>
                 <div className="ProductListTopOverflow">
-                    <table className="ProductTable">
-                        <thead style={{visibility:'collapse'}}>
-                            <tr>
-                            <th style={{width:'10%'}}/>
-                            <th style={{width:'10%'}}/>
-                            <th style={{width:'40%'}}/>
-                            <th style={{width:'17%'}}/>
-                            <th style={{width:'6%'}}/>
-                            <th style={{width:'17%'}}/>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {totalList.data.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.status}</td>
-                                <td>{item.code}</td>
-                                <td>{item.name}</td>
-                                {item.status === '할인' ? 
-                                <td>{item.price.toLocaleString()}</td>
-                                : <td>{item.price.toLocaleString()}원</td>
-                                }
-                                <td>{item.quantity}</td>
-                                {item.status === '할인' ? 
-                                <td>{item.total.toLocaleString()}</td>
-                                : <td>{item.total.toLocaleString()}원</td>
-                                }
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    {totalList.data.map((item, index) => (
+                    <div key={index}>
+                         <div className="MenuInfo">
+                            <p className="Width10P">{item.status}</p>
+                            <p className="Width10P">{item.code}</p>
+                            <p className="Width40P">{item.name}</p>
+                            {item.status === '할인' ? 
+                                <p className="Width15P">{item.price.toLocaleString()}</p>
+                                : <p className="Width15P">{item.price.toLocaleString()}원</p>
+                            }
+                            <p className="Width10P">{item.quantity}</p>
+                            {item.status === '할인' ? 
+                                <p className="Width15P">{item.total.toLocaleString()}</p>
+                                : <p className="Width15P">{item.total.toLocaleString()}원</p>
+                            }
+                        </div>
+                        <hr/>
+                    </div>
+                    ))}
                 </div>
             </div>
             <div className="TotalInfo">
@@ -129,6 +129,7 @@ const ProductList =() =>{
                     <p id="rightP">{total.money}</p>
                 </div>
             </div>
+            {falseModal && <NoListModal onClose={() => {setFalseModal(false)}}/>}
             {modal&& <CouponModal 
                 props={data}
                 onClose={(coupon) => {
