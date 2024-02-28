d73e40e08ad695accc5e6a6f7300161bfe862a03
kimeunseo58 <alan0221@naver.com>
2024-02-28 10:51:31 +0900

    feat: 로그 추가

--- src/components/Payment/ProductList.tsx
+++ src/components/Payment/ProductList.tsx
@@ -37,18 +37,8 @@ const ProductList =() =>{
         };
       }, [socket]);
 
-    useEffect(() => {
-        socket.onopen = (event) => {
-          console.log("WebSocket connection opened:", event);
-        };
-        return () => {
-          socket.onclose = function (event) {
-            console.log("WebSocket connection closed:", event);
-          };
-        };
-      }, [socket]);
-
     socket.onmessage=(e)=>{
+        console.log("socket", e)
         const getCouponList= async() =>{
             try{
                 const res = await get(`${process.env.REACT_APP_API_URL}/pos/coupon/list/${e.data}`);
